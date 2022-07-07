ebfd5ebc28216e6d242f107cd94019b86109acea
Xavier van Heule <59011468+xvheule@users.noreply.github.com>
2022-07-07 10:31:55 +0100

    Implement the console log

--- src/xvh/d_ic_hd/gui/Gui.py
+++ src/xvh/d_ic_hd/gui/Gui.py
@@ -1,15 +1,22 @@
 from CameraElement import CameraElement
+from datetime import date, datetime
 from xvh.d_ic_hd.cameras.CameraList import CameraList
 import Tkinter as Tk
 
 
 class Gui:
     def launch_gui(self):
+        self.log("DI-C-HD Launched Successfully")
         self.gui.mainloop()
 
     def on_press(self):
         self.camera_1.refresh_image()
         self.camera_2.refresh_image()
+
+    def log(self, line):
+        self.console.configure(state="normal")
+        self.console.insert(Tk.END, "[" + str(date.today()) + "][" + datetime.now().strftime("%H:%M:%S") + "] " + line)
+        self.console.configure(state="disabled")
 
     def __init__(self):
         # Create the gui window
@@ -39,6 +46,15 @@ class Gui:
         self.frm_console = Tk.Frame(master=self.frm_output, height=h_console, relief=Tk.GROOVE, borderwidth=3)
         self.frm_cameras.pack(fill=Tk.BOTH, side=Tk.TOP, expand=True)
         self.frm_console.pack(fill=Tk.X, side=Tk.TOP, expand=False)
+
+        # Create the console
+        self.console = Tk.Text(master=self.frm_console)
+        self.console_scroll = Tk.Scrollbar(master=self.frm_console, orient=Tk.VERTICAL, command=self.console.yview)
+        self.frm_console.grid_columnconfigure(0, weight=1)
+        self.console.grid(row=0, column=0, sticky=(Tk.N, Tk.W, Tk.E, Tk.S))
+        self.console_scroll.grid(row=0, column=1, sticky=(Tk.N, Tk.S))
+        self.console['yscrollcommand'] = self.console_scroll.set
+        self.console.configure(state="disabled")
 
         # Create the camera elements
         self.camera_1 = CameraElement(self.frm_cameras, "CAM 1", self.cameras.create_camera(0))
--- src/xvh/d_ic_hd/main.py
+++ src/xvh/d_ic_hd/main.py
@@ -1,4 +1,9 @@
 from gui.Gui import Gui
 
+# Launch the gui
 gui = Gui()
 gui.launch_gui()
+
+
+def log(msg):
+    gui.log(msg)
