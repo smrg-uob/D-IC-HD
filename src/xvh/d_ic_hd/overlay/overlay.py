f0b74f53f0ba85842bed35e9e72cf7eedd293024
Xavier van Heule <59011468+xvheule@users.noreply.github.com>
2022-07-12 08:56:25 +0100

    adjust overlay

--- src/xvh/d_ic_hd/overlay/overlay.py
+++ src/xvh/d_ic_hd/overlay/overlay.py
@@ -8,7 +8,7 @@ h_r = 0.37
 
 def get_rib(points, pitches):
     # Create x, y and z arrays for the ribs
-    t = np.arange(0, 1, (pitches + 0.0)/(0.0 + 2*points))
+    t = np.arange(-0.5, 0.5, (pitches + 0.0)/(0.0 + 2*points))
     alpha = np.multiply(t, 2*np.pi*pitches/n_r)
     x = np.multiply(t, pitches)
     y = np.multiply(np.cos(alpha), r_c + h_r)
--- src/xvh/d_ic_hd/overlay_test.py
+++ src/xvh/d_ic_hd/overlay_test.py
@@ -1,10 +1,8 @@
 from overlay import overlay
-from mpl_toolkits import mplot3d
-import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 
-x, y, z = overlay.get_rib(1000, 4)
+x, y, z = overlay.get_rib(1000, 2)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d', adjustable='box')
